48ad27e4e7207804a70b6a6b28b450aab37686a9
tferguson1028 <tferguson1028@outlook.com>
2023-11-02 23:56:24 -0400

    Up to task 5.6

--- script.js
+++ script.js
@@ -9,11 +9,22 @@ topMenuEl.style.backgroundColor = "var(--top-menu-bg)";
 topMenuEl.classList.add("flex-around");
 
 // Menu data structure
-var menuLinks = [
+let menuLinks = [
   {text: 'about', href: '/about'},
-  {text: 'catalog', href: '/catalog'},
-  {text: 'orders', href: '/orders'},
-  {text: 'account', href: '/account'},
+  {text: 'catalog', href: '#', subLinks: [
+    {text: 'all', href: '/catalog/all'},
+    {text: 'top selling', href: '/catalog/top'},
+    {text: 'search', href: '/catalog/search'},
+  ]},
+  {text: 'orders', href: '#' , subLinks: [
+    {text: 'new', href: '/orders/new'},
+    {text: 'pending', href: '/orders/pending'},
+    {text: 'history', href: '/orders/history'},
+  ]},
+  {text: 'account', href: '#', subLinks: [
+    {text: 'profile', href: '/account/profile'},
+    {text: 'sign out', href: '/account/signout'},
+  ]},
 ];
 
 menuLinks.forEach((link) => 
@@ -23,6 +34,53 @@ menuLinks.forEach((link) =>
   anchorEl.textContent = link.text;
   topMenuEl.appendChild(anchorEl);
 });
+
+const subMenuEl = document.querySelector("#sub-menu");
+subMenuEl.style.height = "100%";
+subMenuEl.style.backgroundColor = "var(--sub-menu-bg)";
+subMenuEl.classList.add("flex-around");
+subMenuEl.style.position = "absolute";
+subMenuEl.style.top = "0";
+
+let topMenuLinks = topMenuEl.querySelectorAll("a");
+let showingSubMenu = false;
+
+topMenuEl.addEventListener("click", function(event)
+{
+  event.preventDefault();
+  event.stopPropagation();
+  let targetEl = event.target;
+  if(targetEl.tagName !== "A")
+    return;
+    
+  console.log(targetEl);
+  if(targetEl.classList.contains("active"))
+  {
+    targetEl.classList.remove("active");
+    showingSubMenu = false;
+    subMenuEl.style.top = "0";
+    return;
+  }
+  
+  topMenuLinks.forEach((a) => a.classList.remove("active"));
+  targetEl.classList.add("active");
+  
+  // This works, yay
+  for(let link of menuLinks)
+  {
+    if(link.text === targetEl.textContent && link.subLinks !== undefined)
+    {
+      showingSubMenu = true;
+      break;
+    }else
+      showingSubMenu = false;
+  }
+  console.log(showingSubMenu);
+  
+  
+});
+
+
 
 
 
